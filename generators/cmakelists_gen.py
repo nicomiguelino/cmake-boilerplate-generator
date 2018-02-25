3d41922bf633c2e90cca5d6329b1892cf31e47ce
nicomiguelino2014 <nicomiguelino2014@gmail.com>
2018-02-25 15:11:06 +0800

    Refactoring for readability.
    DESCRIPTION:
    - Refactored file and path constants into a
    separate module (new file).
    - Removed class enum for FileMode. Used string for 'r' and 'w' instead
    for natural readability.

--- generators/cmakelists_gen.py
+++ generators/cmakelists_gen.py
@@ -1,22 +1,14 @@
 from os import chdir, mkdir, path
-from shutil import rmtree
 from string import Template
 
-PROJECT_ROOT_PATH = path.abspath('.')
-CMAKELISTS_INPUT_PATH = path.abspath('templates/CMakeLists.txt')
-CMAKELISTS_FILENAME = 'CMakeLists.txt'
-GENERATED_FILES_PATH = path.join(PROJECT_ROOT_PATH, 'generated_files')
-
-class FileMode:
-    READ_ONLY = 'r'
-    WRITE_ONLY = 'w'
+from utilities.constants.FileConstants import *
 
 class CMakeListsGenerator:
     def __init__(self, context):
         self.context = context
 
     def generate_file(self):
-        with open(CMAKELISTS_INPUT_PATH, FileMode.READ_ONLY) as input_file_handler:
+        with open(CMAKELISTS_INPUT_PATH, 'r') as input_file_handler:
             template_handler = Template(input_file_handler.read())
             template_output = template_handler.substitute(self.context)
 
@@ -25,5 +17,5 @@ class CMakeListsGenerator:
 
             chdir(GENERATED_FILES_PATH)
 
-            with open(CMAKELISTS_FILENAME, FileMode.WRITE_ONLY) as output_file_handler:
+            with open(CMAKELISTS_FILENAME, 'w') as output_file_handler:
                 output_file_handler.write(template_output)
--- generators/maincpp_gen.py
+++ generators/maincpp_gen.py
@@ -1,22 +1,14 @@
 from os import chdir, mkdir, path
-from shutil import rmtree
 from string import Template
 
-PROJECT_ROOT_PATH = path.abspath('.')
-MAIN_CPP_INPUT_PATH = path.abspath('templates/main.cpp')
-MAIN_CPP_FILENAME = 'main.cpp'
-GENERATED_FILES_PATH = path.join(PROJECT_ROOT_PATH, 'generated_files')
-
-class FileMode:
-    READ_ONLY = 'r'
-    WRITE_ONLY = 'w'
+from utilities.constants.FileConstants import *
 
 class MainCppGenerator:
     def __init__(self):
         self.context = dict()
 
     def generate_file(self):
-        with open(MAIN_CPP_INPUT_PATH, FileMode.READ_ONLY) as input_file_handler:
+        with open(MAIN_CPP_INPUT_PATH, 'r') as input_file_handler:
             template_handler = Template(input_file_handler.read())
             template_output = template_handler.substitute(self.context)
 
@@ -25,5 +17,5 @@ class MainCppGenerator:
 
             chdir(GENERATED_FILES_PATH)
 
-            with open(MAIN_CPP_FILENAME, FileMode.WRITE_ONLY) as output_file_handler:
+            with open(MAIN_CPP_FILENAME, 'w') as output_file_handler:
                 output_file_handler.write(template_output)
