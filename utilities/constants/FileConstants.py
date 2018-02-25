97f7097ec7c0c25cefc8a6978bf76f0fe3c91f25
nicomiguelino2014 <nicomiguelino2014@gmail.com>
2018-02-25 15:16:02 +0800

    Deleted duplicate constants. Removed obvious comments.

--- utilities/constants/FileConstants.py
+++ utilities/constants/FileConstants.py
@@ -1,13 +1,10 @@
 from os import path
 
-# CMakeLists.txt generator constants
 PROJECT_ROOT_PATH = path.abspath('.')
-CMAKELISTS_INPUT_PATH = path.abspath('templates/CMakeLists.txt')
-CMAKELISTS_FILENAME = 'CMakeLists.txt'
 GENERATED_FILES_PATH = path.join(PROJECT_ROOT_PATH, 'generated_files')
 
-# main.cpp generator constants
-PROJECT_ROOT_PATH = path.abspath('.')
+CMAKELISTS_INPUT_PATH = path.abspath('templates/CMakeLists.txt')
+CMAKELISTS_FILENAME = 'CMakeLists.txt'
+
 MAIN_CPP_INPUT_PATH = path.abspath('templates/main.cpp')
 MAIN_CPP_FILENAME = 'main.cpp'
-GENERATED_FILES_PATH = path.join(PROJECT_ROOT_PATH, 'generated_files')
